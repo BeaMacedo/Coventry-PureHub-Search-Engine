29af859198f29433f56ef97d127f10df36c2daf0
BeaMacedo <a.beatrizm2003@gmail.com>
2025-04-14 22:11:01 +0100

    asdfghj

--- app2.py
+++ app2.py
@@ -10,6 +10,269 @@ from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
 from nltk import pos_tag
+import math as m
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def operacoes_logicas(input_text, operator_val, search_type, stem_lema):
+    if stem_lema == 2:  # se lematizacao
+        pub_index = pub_index_lemma
+        pub_list_first = pub_list_first_lemma
+        abstract_index = pub_abstract_index_lemma
+        pub_abstract_list_first = pub_abstract_list_first_lemma
+        author_index = author_index_lemma
+        author_list_first = author_list_first_lemma
+    else:
+        pub_index = pub_index_stem
+        pub_list_first = pub_list_first_stem
+        abstract_index = pub_abstract_index_stem
+        pub_abstract_list_first = pub_abstract_list_first_stem
+        author_index = author_index_stem
+        author_list_first = author_list_first_stem
+
+    output_data = {}
+    if operator_val == 2:  # OPERADOR OR
+        input_text = input_text.lower().split()
+        pointer = []
+        stem_word_file1 = []
+        temp_file = []
+
+        for token in input_text:
+            stem_temp = ""
+            stem_word_file1 = []  # query
+            temp_file1 = []
+            pointer1 = []
+            word_list = word_tokenize(token)
+
+            if stem_lema == 1:
+                for x in word_list:
+                    if x not in stop_words:
+                        stem_temp += stemmer.stem(x) + " "
+            elif stem_lema == 2:
+                stem_temp = enhanced_lemmatize(' '.join([w.lower() for w in word_list if w.lower() not in stop_words]))
+
+            stem_word_file1.append(stem_temp.strip())
+
+            if search_type == "publication" and pub_index.get(stem_word_file1[0].strip()): #Se for "publication", pesquisa no pub_index
+                pointer1 = pub_index.get(stem_word_file1[0].strip())
+                #print(pointer)
+            elif search_type == "author" and author_index.get(stem_word_file1[0].strip()): #Se for "author", pesquisa no author_index
+                pointer1 = author_index.get(stem_word_file1[0].strip())
+            elif search_type == "abstract" and abstract_index.get(stem_word_file1[0].strip()): #Se for "author", pesquisa no author_index
+                pointer1 = abstract_index.get(stem_word_file1[0].strip())
+
+
+            if len(pointer1) == 0:
+                output_data = {}
+            else:
+                for j in pointer: #indice de cada documento que contem a palavra
+                    if search_type == "publication":
+                        temp_file1.append(pub_list_first[j]) #texto do ficheiro publication_list_stemmed.json que é o texto do documento sem stop words e com stem
+                    elif search_type == "author":
+                        temp_file1.append(author_list_first[j])
+                    elif search_type == "abstract":
+                        temp_file1.append(pub_abstract_list_first[j])
+
+            temp_file += temp_file1
+            pointer += pointer1
+
+        return temp_file, stem_word_file1, pointer
+
+    elif operator_val == 1:  # AND
+        input_text = input_text.lower().split()
+        pointer = []
+        match_word = []
+        for token in input_text:
+            temp_file = []
+            set2 = set()
+            stem_word_file = []
+            word_list = word_tokenize(token)
+            stem_temp = ""
+            if stem_lema == 1:
+                for x in word_list:
+                    if x not in stop_words:
+                        stem_temp += stemmer.stem(x) + " "
+            else:
+                stem_temp = enhanced_lemmatize(' '.join([w.lower() for w in word_list if w.lower() not in stop_words]))
+            stem_word_file.append(stem_temp.strip())
+
+            if search_type == "publication" and pub_index.get(stem_word_file[0].strip()):
+                set1 = set(pub_index.get(stem_word_file[0].strip())) #set 1 é o conjunto dos indices dos documentos onde a palavra processada aparece
+                pointer.extend(list(set1)) #Adiciona o conjunto set1 ao pointer
+            elif search_type == "author" and author_index.get(stem_word_file[0].strip()):
+                set1 = set(author_index.get(stem_word_file[0].strip()))
+                pointer.extend(list(set1))
+            elif search_type == "abstract" and abstract_index.get(stem_word_file[0].strip()):
+                set1 = set(abstract_index.get(stem_word_file[0].strip()))
+                pointer.extend(list(set1)) #adiciona os indices dos documentos onde aparece o token em questão
+
+
+            if match_word == []:
+                match_word = list(
+                    {z for z in pointer if z in set2 or (set2.add(z) or False)}
+                )
+            else:
+                match_word.extend(list(set1))
+                match_word = list(
+                    {z for z in match_word if z in set2 or (set2.add(z) or False)}
+                )
+
+        if len(input_text) > 1:
+            match_word = {z for z in match_word if z in set2 or (set2.add(z) or False)}
+            if len(match_word) == 0:
+                output_data = {}
+
+            else:
+                for j in list(match_word):
+                    if search_type == "publication":
+                        temp_file.append(pub_list_first[j]) #texto completo dos documentos sem stop words e com stem
+                    elif search_type == "author":
+                        temp_file.append(author_list_first[j])
+                    elif search_type == "abstract":
+                        temp_file.append(pub_abstract_list_first[j])
+            return temp_file, stem_word_file, list(match_word)
+
+        else:
+            if len(pointer) == 0:
+                output_data = {}
+            else:
+                for j in pointer:
+                    if search_type == "publication":
+                        temp_file.append(pub_list_first[j])
+                    elif search_type == "author":
+                        temp_file.append(author_list_first[j])
+                    elif search_type == "abstract":
+                        temp_file.append(pub_abstract_list_first[j])
+
+            return temp_file, stem_word_file, pointer
+
+    elif operator_val == 3:  # OPERADOR NOT
+        input_text = input_text.lower().split()
+        pointer = []
+        match_word = []
+        complete_set = []
+        to_remove = []
+        temp_file = []
+        stem_word_file = []
+        if search_type == "publication":
+            complete_set = list(
+                set(value for values in pub_index.values() for value in values)
+            )  # lista de todos os indices dos docs
+
+        elif search_type == "author":
+            complete_set = list(
+                set(value for values in author_index.values() for value in values)
+            )  # lista de todos os indices dos docs
+
+        elif search_type == "abstract":
+            complete_set = list(
+                set(value for values in abstract_index.values() for value in values)
+            )  # lista de todos os indices dos docs
+
+        for token in input_text:
+
+            set2 = set()
+
+            word_list = word_tokenize(token)
+            stem_temp = ""
+            if stem_lema == 1:
+                for x in word_list:
+                    if x not in stop_words:
+                        stem_temp += stemmer.stem(x) + " "
+            else:
+                stem_temp = enhanced_lemmatize(' '.join([w.lower() for w in word_list if w.lower() not in stop_words]))
+
+            stem_word_file.append(stem_temp.strip())
+            set1 = set()  # definir a variável
+            if search_type == "publication" and pub_index.get(stem_word_file[0].strip()):
+                set1 = set(
+                    pub_index.get(stem_word_file[0].strip())
+                )  # indices dos docs que fazem match
+
+            elif search_type == "author" and author_index.get(
+                    stem_word_file[0].strip()
+            ):
+                set1 = set(author_index.get(stem_word_file[0].strip()))
+
+            elif search_type == "abstract" and abstract_index.get(
+                    stem_word_file[0].strip()
+            ):
+                set1 = set(abstract_index.get(stem_word_file[0].strip()))
+
+            to_remove += list(set1)
+
+        complete_set = list(set(complete_set) - set(to_remove))
+
+        if len(input_text) > 1:
+            for j in list(complete_set):
+                if search_type == "publication":
+                    temp_file.append(pub_list_first[j])
+                elif search_type == "author":
+                    temp_file.append(author_list_first_stem[j])
+                elif search_type == "abstract":
+                    temp_file.append(pub_abstract_list_first[j])
+
+        else:
+            if len(complete_set) == 0:
+                output_data = {}
+            else:
+                for j in list(complete_set):
+                    if search_type == "title":
+                        temp_file.append(pub_list_first[j])
+                    elif search_type == "author":
+                        temp_file.append(author_list_first_stem[j])
+                    if search_type == "abstract":
+                        temp_file.append(pub_abstract_list_first[j])
+
+        return temp_file, stem_word_file, list(complete_set)
+
+
+def not_operador(word):
+    operadores = ["not", "and", "or"]
+    if word not in operadores:
+        return True
+    else:
+        return False
+
+import re
+def parse_query(query):
+    operadores = ["not", "and", "or"]
+    query = query.lower().strip()
+    operacoes_and1 = []
+    query1 = query
+    while re.search(r"([^\s]+) and ([^\s]+)", query1):
+        query1 = re.sub(r"([^\s]+) and ([^\s]+)", r"\1 \2", query1)
+
+    operacoes_and = []
+    for tuple in operacoes_and1:
+        operacoes_and.append(f"{tuple[0]} {tuple[1]}")
+
+    operacoes_or = re.split(r" or ", query)
+    operacoes_not1 = re.findall(r"not ([^\s]+)", query)
+    operacoes_not = []
+    if operacoes_not1:
+        for operacao in operacoes_not1:
+            if operacao:
+                operacao = operacao.split(" ")
+                operacoes_not.append(operacao[0])
+    or_queries = []
+    for i in range(len(operacoes_or) - 1):
+        t1 = operacoes_or[i].split()
+        termo1 = t1[len(t1) - 1]
+        t2 = operacoes_or[i + 1].split()
+        termo2 = t2[0]
+        or_queries.append(termo1 + " " + termo2)
+
+    query2 = query1.split()
+    for i in range(len(query2) - 1):
+        if not_operador(query2[i]) and not_operador(query2[i + 1]):
+            operacoes_and.append(f"{query2[i]} {query2[i + 1]}")
+    print("look here ::: " + str(operacoes_and) + "----" + str(operacoes_not) + "-----" + str(or_queries))
+
+    return operacoes_and, operacoes_not, or_queries
+
+
 
 nltk.download('stopwords')
 nltk.download('punkt')
@@ -146,7 +409,7 @@ def search_data(input_text, operator_val, search_type, stem_lema): #função de 
                     output_data[j] = cosine_output[pointer.index(j)]
                 print(output_data)
 
-    else:  # operador and
+    elif operator_val == 1:  # operador and
         input_text = input_text.lower().split()
         pointer = [] #documentos encontrados
         match_word = [] # Apenas os documentos que contêm TODAS as palavras (interseção)
@@ -225,7 +488,142 @@ def search_data(input_text, operator_val, search_type, stem_lema): #função de 
                 for j in pointer:
                     output_data[j] = cosine_output[pointer.index(j)]
 
+    elif operator_val == 3:  # OPERADOR NOT
+        input_text = input_text.lower().split()
+        pointer = []
+        match_word = []
+        complete_set = []
+        to_remove = []
+
+        if search_type == "publication":
+            complete_set = list(
+                set(value for values in pub_index.values() for value in values)
+            )  # lista de todos os indices dos docs
+
+        elif search_type == "author":
+            complete_set = list(
+                set(value for values in author_index.values() for value in values)
+            )  # lista de todos os indices dos docs
+
+        elif search_type == "abstract":
+            complete_set = list(
+                set(value for values in abstract_index.values() for value in values)
+            )  # lista de todos os indices dos docs
+
+        for token in input_text:
+
+            temp_file = []
+            set1 = set()
+            set2 = set()
+            stem_word_file = []
+            word_list = word_tokenize(token)
+            stem_temp = ""
+
+            if stem_lema == 1:
+                for x in word_list:
+                    if x not in stop_words:
+                        stem_temp += stemmer.stem(x) + " "
+            else:
+                stem_temp = enhanced_lemmatize(' '.join([w.lower() for w in word_list if w.lower() not in stop_words]))
+
+            stem_word_file.append(stem_temp.strip())
+            if search_type == "publication" and pub_index.get(stem_word_file[0].strip()):
+                set1 = set(
+                    pub_index.get(stem_word_file[0].strip())
+                )  # indices dos docs que fazem match
+
+            elif search_type == "author" and author_index.get(
+                    stem_word_file[0].strip()
+            ):
+                set1 = set(author_index.get(stem_word_file[0].strip()))
+
+            elif search_type == "abstract" and abstract_index.get(
+                    stem_word_file[0].strip()
+            ):
+                set1 = set(abstract_index.get(stem_word_file[0].strip()))
+
+        if len(input_text) > 0:
+            for j in list(complete_set):
+                if search_type == "publication":
+                    temp_file.append(pub_list_first[j])
+                elif search_type == "author":
+                    temp_file.append(author_list_first_stem[j])
+                elif search_type == "abstract":
+                    temp_file.append(pub_abstract_list_first[j])
+
+            temp_file = tfidf.fit_transform(temp_file)
+            cosine_output = cosine_similarity(temp_file, tfidf.transform(stem_word_file))
+            for j in list(complete_set):
+                output_data[j] = cosine_output[list(complete_set).index(j)]
+
+
+        else:
+            if len(complete_set) == 0:
+                output_data = {}
+            else:
+                for j in list(complete_set):
+                    if search_type == "title":
+                        temp_file.append(pub_list_first[j])
+                    elif search_type == "author":
+                        temp_file.append(author_list_first_stem[j])
+                    if search_type == "abstract":
+                        temp_file.append(pub_abstract_list_first[j])
+
+                temp_file = tfidf.fit_transform(temp_file)
+                cosine_output = cosine_similarity(temp_file, tfidf.transform(stem_word_file))
+                for j in list(complete_set):
+                    output_data[j] = cosine_output[list(complete_set).index(j)]
+
+
+    elif operator_val == 4:  # PESQUISA COM OPERADORES LOGICOS
+        temp_file = []
+        stem_word_file = []
+        output_data = {}
+        pointer = []
+        if len(input_text.split()) > 1:
+            operacoes_and, operacoes_not, operacoes_or = parse_query(input_text)
+            for ope in operacoes_not:
+                temp_file1, stem_word_file1, pointer1 = operacoes_logicas(
+                    ope, 3, search_type, stem_lema
+                )
+                temp_file += temp_file1
+                stem_word_file = stem_word_file1
+                pointer += pointer1
+
+
+            for ope in operacoes_or:
+                temp_file1, stem_word_file1, pointer1 = operacoes_logicas(
+                    ope, 2, search_type, stem_lema
+                )
+                temp_file += temp_file1
+                stem_word_file = stem_word_file1
+                pointer += pointer1
+
+
+            # EXECUTAR OPERACOES AND
+            for ope in operacoes_and:
+                temp_file1, stem_word_file1, pointer1 = operacoes_logicas(
+                    ope, 1, search_type, stem_lema
+                )
+                temp_file += temp_file1  # ANTES ESTAVA ASSIM   ----- > list(set(temp_file1 + temp_file))
+                stem_word_file = stem_word_file1
+                if len(pointer) != 0:
+                    pointer = list((set(pointer).intersection(set(pointer1))))
+                else:
+                    pointer = pointer1
+
+        else:
+            temp_file, stem_word_file, pointer = operacoes_logicas(
+                input_text, 2, search_type, stem_lema
+            )
+
+        temp_file = tfidf.fit_transform(temp_file)
+        cosine_output = cosine_similarity(temp_file, tfidf.transform(stem_word_file))
+        for j in pointer:
+            output_data[j] = cosine_output[pointer.index(j)]
+
     return output_data
+
 
 #-----------------para indices pdf do grupo de pesquisa LIB
 # Função para carregar os índices mapeados de dic_indices_pdfs.json
@@ -361,7 +759,6 @@ def search_LIB_data(input_text, operator_val):
 
     return output_data
 
-
 #inicial
 def app():  # interface Streamlit
     # Load the image and display it
@@ -371,7 +768,7 @@ def app():  # interface Streamlit
     input_text = st.text_input("Search research:", key="query_input")
     operator_val = st.radio(
         "Search Filters",
-        ['Exact', 'Relevant'],
+        ["AND", "OR", "NOT", "Logical operators"],
         index=1,
         key="operator_input",
         horizontal=True,
@@ -394,13 +791,28 @@ def app():  # interface Streamlit
 
     if st.button("SEARCH"):
         if search_type == "Publications":
-            output_data = search_data(input_text, 1 if operator_val == 'Exact' else 2, "publication",  1 if stem_lema == "Stemming" else 2 )
+            output_data = search_data(input_text, 1 if operator_val == 'AND' else (
+                            2
+                            if operator_val == "OR"
+                            else (3 if operator_val == "NOT" else 4)
+                        ), "publication",  1 if stem_lema == "Stemming" else 2 )
+
             show_results(output_data, search_type)
         elif search_type == "Authors":
-            output_data = search_data(input_text, 1 if operator_val == 'Exact' else 2, "author", 1 if stem_lema == "Stemming" else 2 )
+            output_data = search_data(input_text, 1 if operator_val == 'AND' else (
+                            2
+                            if operator_val == "OR"
+                            else (3 if operator_val == "NOT" else 4)
+                        ), "author", 1 if stem_lema == "Stemming" else 2 )
+
             show_results(output_data, search_type)
         elif search_type == "Abstracts":
-            output_data = search_data(input_text, 1 if operator_val == 'Exact' else 2, "abstract", 1 if stem_lema == "Stemming" else 2 )
+            output_data = search_data(input_text, 1 if operator_val == 'AND' else (
+                            2
+                            if operator_val == "OR"
+                            else (3 if operator_val == "NOT" else 4)
+                        ), "abstract", 1 if stem_lema == "Stemming" else 2 )
+
             show_results(output_data, search_type)
         elif search_type == "LIB Mathematics Support Centre Publications Search":
             output_data = search_LIB_data(input_text, 1 if operator_val == 'Exact' else 2)
